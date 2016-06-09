4be5a06746c3009b67a13d630913010bf3345770
威力 <willycha0119@gmail.com>
2016-06-09 20:08:00 +0800

    tiger
    tiger

--- tiger.php
+++ tiger.php
@@ -6,7 +6,7 @@ header("Content-Type:text/html; charset=UTF-8");
 for(var i = 0 ; i < 1 ; i++) {
 	var popup = window.open('http://www.tigerair.com/tw/zh/real_deals.php', '_blank', 'width=500,height=500');
 
-	setTimeout(function(){ console.log(popup.document.documentElement.outerHTML); }, 3000);
+	setTimeout(function(){ alert(popup.document.documentElement.outerHTML); }, 3000);
 
 	
 }
